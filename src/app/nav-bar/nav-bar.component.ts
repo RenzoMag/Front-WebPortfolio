631f5216e9e0f4020ed0a283ad0b24c411511184
RenzoMag <renzomagnoni203@gmail.com>
2023-01-30 15:51:12 -0300

    NavBarsemifinish

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  sepin(sepinout: HTMLElement){
+    sepinout.style.translate = "0px"
+    sepinout.style.transition = "translate 800ms"
+  }
 
+  sepout(sepinout: HTMLElement){
+    sepinout.style.translate = "-2000px"
+    sepinout.style.transition = "translate 3500ms"
+  }
+
+  cerrar(modal: HTMLElement){
+    modal.style.opacity = "0"
+    modal.style.pointerEvents = "none"
+  }
+
+  abrir(modal: HTMLElement){
+    modal.style.opacity = "1"
+    modal.style.pointerEvents = "unset"
+  }
 }
